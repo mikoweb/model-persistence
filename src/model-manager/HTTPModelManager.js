e6e120a589bd88c22de301ee37375694f0c751c4
Rafał Mikołajun <root@rmweb.pl>
2017-12-09 16:11:56 +0100

    fix manager

--- src/model-manager/HTTPModelManager.js
+++ src/model-manager/HTTPModelManager.js
@@ -34,20 +34,14 @@ export default class HTTPModelManager extends ModelManagerInterface {
      */
     save(model, options = {}) {
         return new Promise((resolve, reject) => {
-            let request;
             const requestOptions = this._getRequestOptions({
                 data: this.createOutputTransformer().transform(model)
             });
 
-            if (this._locator.isEmptyModelId(model)) {
-                request = this._client.post(this._locator.locate(model), requestOptions);
-            } else {
-                request = this._client.put(this._locator.locate(model), requestOptions);
-            }
-
-            request.then(() => {
+            const method = this._locator.isEmptyModelId(model) ? this._client.post : this._client.put;
+            method(this._locator.locate(model), requestOptions).then(() => {
                 resolve(true);
-            }).catch((e) => () => {
+            }).catch((e) => {
                 reject(e);
             });
         });
@@ -60,7 +54,7 @@ export default class HTTPModelManager extends ModelManagerInterface {
         return new Promise((resolve, reject) => {
             this._client.delete(this._locator.locate(model), this._getRequestOptions()).then(() => {
                 resolve(true);
-            }).catch((e) => () => {
+            }).catch((e) => {
                 reject(e);
             });
         });
