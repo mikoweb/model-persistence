d81c1cb1a812eeaef777bc60cf2f88cbc60d748c
Rafał Mikołajun <root@rmweb.pl>
2017-12-14 19:38:38 +0100

    test StorageModelManager remove

---
+++ test/model-manager/storage/remove.js
@@ -0,0 +1,51 @@
+describe('model-manager.StorageModelManager', () => {
+    const factory = modelPersist.storageFactory;
+    class Locator extends modelPersist.StorageLocatorAbstract {
+        get basePath() {
+            return '/article';
+        }
+        get storage() {
+            return fakeLocalStorage;
+        }
+    }
+
+    const Model = new modelPersist.Model({
+        id: Number,
+        title: String,
+        content: String
+    });
+
+    const locator = new Locator();
+    const manager = factory.createManager(locator);
+
+    afterEach(() => {
+        locator.storage.clear();
+    });
+
+    it('remove', () => {
+        manager.saveSync(new Model({
+            id: 1,
+            title: 'New Lorem Ipsum',
+            content: '<p>New Lorem Ipsum</p>'
+        }));
+
+        expect(manager.has(1)).to.be.true;
+
+        return manager.remove(manager.getSync(1, Model)).then((removed) => {
+            expect(removed).to.be.true;
+            expect(manager.has(1)).to.be.false;
+        });
+    });
+
+    it('removeSync', () => {
+        manager.saveSync(new Model({
+            id: 1,
+            title: 'New Lorem Ipsum',
+            content: '<p>New Lorem Ipsum</p>'
+        }));
+
+        expect(manager.has(1)).to.be.true;
+        manager.removeSync(manager.getSync(1, Model));
+        expect(manager.has(1)).to.be.false;
+    });
+});
