76183a49924a50ef273df399867584470919ac4d
Rafał Mikołajun <root@rmweb.pl>
2017-12-16 21:10:29 +0100

    add jsdoc-to-markdown

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const Path = require('./gulp/path');
 const rollupBundle = require('./gulp/rollup-bundle');
 const gulp = require('gulp');
 const bundleUglify = require('./gulp/gulp-uglify');
+const fs = require('fs');
+const gutil = require('gulp-util');
+const gulpJsdoc2md = require('gulp-jsdoc-to-markdown');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
 
 gulp.task('build-lib', () => {
     return rollupBundle('./src/index.js', 'model-persistence.js', 'modelPersist', {
@@ -23,6 +28,17 @@ gulp.task('dist-lib', ['build-lib'], () => {
     return bundleUglify(Path.bundle('/model-persistence.js'), Path.bundle());
 });
 
+gulp.task('docs', () => {
+    return gulp.src('src/**/*.js')
+        .pipe(gulpJsdoc2md())
+        .on('error', function (err) {
+            gutil.log(gutil.colors.red('jsdoc2md failed'), err.message)
+        })
+        .pipe(rename(function (path) {
+            path.extname = '.md'
+        }))
+        .pipe(gulp.dest('api'))
+});
 
 gulp.task('dist', ['dist-lib']);
 
